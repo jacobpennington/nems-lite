f2f623c7ddc7acf0b66026841bcd81bdc1015237
jacobpennington <jacob.pennington@wsu.edu>
2022-06-21 11:59:18 -0700

    Added a third WC example using Phi, Variable.

--- nems/modules/weight_channels.py
+++ nems/modules/weight_channels.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from nems.modules.base import Module
+from nems.modules.base import Module, Phi, Variable
 
 
 class WeightChannels(Module):
@@ -41,18 +41,17 @@ class WeightChannels(Module):
         
         """
         self.shape = shape
-        if parameters is None:
-            parameters = self.initial_parameters(parameterization)
-        super.__init__(parameters=parameters, **kwargs)
+        self.parameterization
+        super.__init__(**kwargs)
 
     # TODO: this requires python 3.10. If we don't want to require that
     #       change back to if/else chain. But if we're re-doing everything
     #       anyway, would be nice to take advantage of newer syntax for stuff
     #       like this.
-    def initial_parameters(self, parameterization):
+    def initial_parameters(self):
         parameters = {}
 
-        match parameterization:
+        match self.parameterization:
             case None:
                 # TODO: better default?
                 default = np.zeros(shape=self.shape)
@@ -69,9 +68,7 @@ class WeightChannels(Module):
                 def get_coefficients(self):
                     pass
 
-
         self.get_coefficients = get_coefficients
-        self.parameterization = parameterization
         return parameters
 
 
@@ -82,7 +79,7 @@ class WeightChannels(Module):
     def evaluate(self, *inputs):
         """TODO: docstring, and check ordering on matrix multiplication."""
         coefficients = self.get_coefficients()
-        y = [x @ coefficients for x in inputs]
+        y = [coefficients @ x for x in inputs]
         return y
 
     def tensorflow_layer(self):
@@ -115,18 +112,22 @@ class WeightChannelsV2(Module):
         WeightChannels
         
         """
-        super.__init__(**kwargs)
         self.shape = shape
+        super.__init__(**kwargs)
+
+        # TODO: should these just go in the base Module class?
         if self.parameters is None:
             parameters = self.initial_parameters()
         # TODO: should this be done automatically? maybe expose an option, like
         #       `initialize_to_mean=True`?
         if self.priors is not None:
             parameters = self.mean_of_priors()
+        self.parameters = parameters
 
     # TODO: don't really *need* a separate method in this case since it's so
-    #       simple, but idea is that other Modules might have much more
-    #       complicated defaults. Also this makes it easy to reset to defaults.
+    #       simple, but idea is that other WeightChannels might have much more
+    #       complicated defaults. Also returning instead of setting  makes it
+    #       easy to reset to defaults.
     def initial_parameters(self):
         # TODO: better default?
         parameters = {'coefficients': np.zeros(shape=self.shape)}
@@ -141,15 +142,21 @@ class WeightChannelsV2(Module):
     def evaluate(self, *inputs):
         """TODO: docstring, and check ordering on matrix multiplication."""
         coefficients = self._get_coefficients()
-        y = [x @ coefficients for x in inputs]
+        y = [coefficients @ x for x in inputs]
         return y
 
     def tensorflow_layer(self):
         # TODO
         pass
 
-class GaussianWeightChannels(WeightChannelsV2):
+
+class GaussianWeightChannelsV2(WeightChannelsV2):
     """Same __init__, evaluate, etc"""
+
+    def __init__(**kwargs):
+        """TODO: overwrite docstring for expected parameters but same init."""
+        super.__init__(**kwargs)
+
     def initial_parameters(self):
         # TODO: better defaults? (see old NEMS)
         parameters = {'mean': 0, 'std': 1}
@@ -162,3 +169,91 @@ class GaussianWeightChannels(WeightChannelsV2):
     def tensorflow_layer(self):
         # TODO: tensorflow_layer would probably be different as well.
         pass
+
+
+# TODO: only keep one version, this is just for comparison when discussing
+#       with Stephen.
+class WeightChannelsV3(Module):
+    # Same as V2, but uses the Phi & Variable classes
+
+    def __init__(self, shape, **kwargs):
+        """TODO: docstring
+
+        Parameters
+        ----------
+        shape : 2-tuple
+            First entry specifies the expected spectral dimension of the input,
+            second entry is the spectral dimension of the output. Shape will be
+            used to initialize the `'coefficients'` Variable if parameters=None.
+        parameters : Phi or None, optional
+            Specifies the value of each variable used to compute
+            `WeightChannels.evaluate()`. If `None`, values will be determined by
+            `WeightChannels.initial_parameters`.
+            Expected format: Phi(Variable(name='coefficients', shape=shape))
+        
+        Returns
+        -------
+        WeightChannels
+        
+        """
+        self.shape = shape
+        super.__init__(**kwargs)
+
+    def initial_parameters(self):
+        coefficients = Variable(name='coefficients', shape=self.shape)
+        return Phi(coefficients)
+
+    @property
+    def coefficients(self):
+        return self.parameters['coefficients'].values
+
+    def evaluate(self, *inputs):
+        """TODO: docstring, and check ordering on matrix multiplication."""
+        y = [self.coefficients @ x for x in inputs]
+        return y
+
+
+class GaussianWeightChannelsV3(WeightChannelsV3):
+    # Same as V2, but uses the Phi & Variable classes
+
+    def __init__(**kwargs):
+        """TODO: overwrite docstring for expected parameters but same init."""
+        super.__init__(**kwargs)
+
+    def initial_parameters(self):
+        n_output_channels, _ = self.shape
+        shape = (n_output_channels,)
+        parameters = Phi(
+            Variable(name='mean', shape=shape, bounds=(0, 1)),
+            Variable(name='std', shape=shape, bounds=(0, None))
+            # NOTE: bounds aren't actually set up for Variables yet, but this
+            #       is an example of why I think it would be an intuitive place
+            #       for them (can still pass through Modules, but they would
+            #       ultimately be implemented in the Variable class).
+            )
+
+        return parameters
+
+    # TODO: question for Stephen. Have we tried this with an added "vertical"
+    #       shift to allow negative coefficients at the edges? I.e. if peak of
+    #       gaussian is at BF, a narrowband sound would get big excitation but
+    #       broadband would get a reduced response.
+    @property
+    def coefficients(self):
+        """Return N discrete gaussians with T bins, where `self.shape=(N,T)`."""
+        mean = self.parameters['mean'].values
+        std = self.parameters['std'].values
+        _, n_input_channels = self.shape
+
+        x = np.arange(n_input_channels)/n_input_channels
+        mean = np.asanyarray(mean)[..., np.newaxis]
+        sd = np.asanyarray(sd)[..., np.newaxis]
+        coefficients = np.exp(-0.5*((x-mean)/sd)**2)
+
+        # Normalize by the cumulative sum for each channel
+        cumulative_sum = np.sum(coefficients, axis=1, keepdims=True)
+        # If all coefficients for a channel are 0, skip normalization
+        cumulative_sum[cumulative_sum == 0] = 1
+        coefficients /= cumulative_sum
+
+        return coefficients
