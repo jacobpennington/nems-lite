0888bf7e8f62ff4ca1f9f16f188fbf7f19485ab1
jacobpennington <jacob.pennington@wsu.edu>
2022-08-31 17:05:54 -0700

    added index tests for model

--- tests/models/test_model.py
+++ tests/models/test_model.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from nems import Model
 from nems.layers import WeightChannels, FiniteImpulseResponse, DoubleExponential
+from nems.layers.base import Layer
 
 
 def test_constructor():
@@ -31,6 +32,25 @@ def test_add_layers():
     # correct order?
     assert model2.layers[0] == layers[0]
     assert model2.layers[1] == layers[1]
+
+
+def test_indexing():
+    model = Model()
+    model.add_layers(
+        Layer(name='zero', input='zero'), Layer(name='one', input='one'),
+        Layer(name='two', input='two')
+    )
+
+    # Index int
+    assert model.layers[0].name == 'zero'
+    assert model.layers[-1].name == 'two'
+    # Multiple ints
+    assert model.layers[0, 1][1].name == 'one'
+    # String
+    assert model.layers['zero'].input == 'zero'
+    assert len(model.layers['one', 'two']) == 2
+    # Slice
+    assert len(model.layers[0:None]) == 3
 
 
 def test_freeze_layers(spectrogram):
@@ -72,3 +92,25 @@ def test_freeze_layers(spectrogram):
     assert len(model.get_parameter_vector(as_list=True)) == p_count
     # Evaluation shouldn't change.
     assert np.allclose(out, unfrozen_out)
+
+
+def test_get_set():
+    wc_zero = np.zeros(shape=(5, 3))
+    fir_one = np.ones(shape=(10, 3))
+    wc = WeightChannels(shape=(5, 3), name='wc')
+    fir = FiniteImpulseResponse(shape=(10, 3), name='fir')
+
+    model = Model(layers=[wc, fir]).sample_from_priors()
+    values = model.get_parameter_values()
+    # Fetching the correct values?
+    assert np.allclose(values['wc']['coefficients'],
+                       model.layers['wc'].get_parameter_values())
+    assert np.allclose(values['fir']['coefficients'],
+                       model.layers['fir'].get_parameter_values())
+
+    model.set_parameter_values(
+        {'wc': {'coefficients': wc_zero}, 'fir': {'coefficients': fir_one}}
+        )
+    # Setting correct values?
+    assert np.allclose(model.layers['wc'].get_parameter_values(), wc_zero)
+    assert np.allclose(model.layers['fir'].get_parameter_values(), fir_one)
