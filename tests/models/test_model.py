cd07f6867a835f3b1c96c8c8b89c48c7175e233b
jacobpennington <jacob.pennington@wsu.edu>
2022-08-31 19:50:42 -0700

    Added some tests for Model.evaluate

--- tests/models/test_model.py
+++ tests/models/test_model.py
@@ -1,6 +1,8 @@
+import pytest
 import numpy as np
 
 from nems import Model
+from nems.models.dataset import DataSet
 from nems.layers import WeightChannels, FiniteImpulseResponse, DoubleExponential
 from nems.layers.base import Layer
 
@@ -53,6 +55,72 @@ def test_indexing():
     assert len(model.layers[0:None]) == 3
 
 
+def test_get_set():
+    wc_zero = np.zeros(shape=(5, 3))
+    fir_one = np.ones(shape=(10, 3))
+    wc = WeightChannels(shape=(5, 3), name='wc')
+    fir = FiniteImpulseResponse(shape=(10, 3), name='fir')
+
+    model = Model(layers=[wc, fir]).sample_from_priors()
+    values = model.get_parameter_values()
+    # Fetching the correct values?
+    assert np.allclose(values['wc']['coefficients'],
+                       model.layers['wc'].get_parameter_values())
+    assert np.allclose(values['fir']['coefficients'],
+                       model.layers['fir'].get_parameter_values())
+
+    model.set_parameter_values(
+        {'wc': {'coefficients': wc_zero}, 'fir': {'coefficients': fir_one}}
+        )
+    # Setting correct values?
+    assert np.allclose(model.layers['wc'].get_parameter_values(), wc_zero)
+    assert np.allclose(model.layers['fir'].get_parameter_values(), fir_one)
+
+
+class TestEvaluate:
+
+    def test_evaluate(self, spectrogram, state):
+        time, spectral = spectrogram.shape
+        _, n_states = state.shape
+        # Basic sequential model, each layer should pass output to the next one
+        # and state should be automatically added to inputs for stategain.
+        model = Model.from_keywords(
+            f'wc.{spectral}x1-fir.15x1-dexp.1-stategain.{n_states}x1'
+            )
+        out = model.evaluate(spectrogram, state=state)
+        # Should be one output, same number of time bins
+        assert out[DataSet.default_output].shape == (time, 1)
+        # state kwarg gets mapped automatically since StateGain.input is None.
+        assert model.layers[-1].data_map.kwargs == \
+            {'state': DataSet.default_state}
+
+    def test_evaluate_batches(self, spectrogram, state_with_samples):
+        # Prepend singleton sample axis.
+        # This should broadcast to number of state samples.
+        spectrogram = spectrogram[np.newaxis, ...]
+        spectral = spectrogram.shape[-1]
+        samples, time, n_states = state_with_samples.shape
+        model = Model.from_keywords(
+            f'wc.{spectral}x1-fir.15x1-dexp.1-stategain.{n_states}x1'
+            )
+
+        with pytest.raises(Exception):
+            # default batch_size=0 not compatible with sample dimension.
+            model.evaluate(spectrogram, state=state_with_samples)
+
+        out1 = model.evaluate(spectrogram, state=state_with_samples,
+                              batch_size=None)
+        assert out1[DataSet.default_output].shape == (samples, time, 1)
+        # Even though spectrogram broadcasts, the original shape should be
+        # preserved in the output.
+        assert out1[DataSet.default_input].shape == (1, time, spectral)
+
+
+def test_fit(spectrogram, response, spectrogram_with_samples,
+             response_with_samples):
+    pass  # TODO
+
+
 def test_freeze_layers(spectrogram):
     time, spectral = spectrogram.shape
     wc = WeightChannels(shape=(spectral, 3))
@@ -93,24 +161,3 @@ def test_freeze_layers(spectrogram):
     # Evaluation shouldn't change.
     assert np.allclose(out, unfrozen_out)
 
-
-def test_get_set():
-    wc_zero = np.zeros(shape=(5, 3))
-    fir_one = np.ones(shape=(10, 3))
-    wc = WeightChannels(shape=(5, 3), name='wc')
-    fir = FiniteImpulseResponse(shape=(10, 3), name='fir')
-
-    model = Model(layers=[wc, fir]).sample_from_priors()
-    values = model.get_parameter_values()
-    # Fetching the correct values?
-    assert np.allclose(values['wc']['coefficients'],
-                       model.layers['wc'].get_parameter_values())
-    assert np.allclose(values['fir']['coefficients'],
-                       model.layers['fir'].get_parameter_values())
-
-    model.set_parameter_values(
-        {'wc': {'coefficients': wc_zero}, 'fir': {'coefficients': fir_one}}
-        )
-    # Setting correct values?
-    assert np.allclose(model.layers['wc'].get_parameter_values(), wc_zero)
-    assert np.allclose(model.layers['fir'].get_parameter_values(), fir_one)
