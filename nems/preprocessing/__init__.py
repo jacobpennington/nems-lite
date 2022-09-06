e256c594c0477c006676bf9226869bbff4c64e8d
jacobpennington <jacob.pennington@wsu.edu>
2022-09-05 22:47:15 -0700

    Added spike_times_to_raster and reverse

--- nems/preprocessing/__init__.py
+++ nems/preprocessing/__init__.py
@@ -4,9 +4,9 @@
 #       the strfy proposal document.
 Contents:
     `spectrogram.py`: Convert sound waveforms to spectrograms.
-    `filter.py`: Transform data using smoothing, high-pass filter, etc.
-    `normalize.py`: Scale data to a standard dynamic range.
-    `rasterize.py`: Convert data to continuous time series (ex: spikes -> PSTH).
+    `filters.py`: Transform data using smoothing, high-pass filter, etc.
+    `normalization.py`: Scale data to a standard dynamic range.
+    `raster.py`: Convert data to continuous time series (ex: spikes -> PSTH).
     `split.py`: Separate data into estimation & validation sets.
     `mask.py`: Exclude subsets of data based on various criteria.
     `merge.py`: Align and combine datasets for fitting a single large model.
@@ -18,4 +18,9 @@ from nems.preprocessing.split import (
     split_at_indices,
     get_jackknife_indices,
     get_jackknife
+    )
+
+from nems.preprocessing.raster import (
+    raster_to_spike_times,
+    spike_times_to_raster
     )
